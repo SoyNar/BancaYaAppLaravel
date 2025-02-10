658ebd806bc714dee8a4699d7d4158bee74de062
SoyNar <narciris.tech@gmail.com>
2025-02-10 14:29:43 -0500

    feat: add changes about userController

---
+++ resources/views/screens/user-screen.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/app')
+
+@section('title', 'Título de la Página')
+
+@section('content')
+    <!-- Contenido específico de la página -->
+    <div class="bg-white rounded-lg shadow p-6">
+        <h2 class="text-2xl font-bold mb-4">Título de la Sección</h2>
+        <!-- Más contenido aquí -->
+    </div>
+@endsection
