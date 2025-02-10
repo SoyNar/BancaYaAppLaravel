5259191df515d385583aad8b26990d96a7498d5b
Nar Mena <narciris.tech@gmail.com>
2025-02-09 20:14:20 -0500

    add client role

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {  $client = User::firstOrCreate(
+        ['email' => 'marina@example.com'],
+        [
+            'name' => 'client',
+            'password' => Hash::make('123456789'),
+        ]
+    );
+
+
+        // Crear roles si no existen
+        $clientRole = Role::firstOrCreate(['name' => 'client']);
+
+        // Asignar roles a los usuarios
+        $client->assignRole($clientRole);
+
+        // Mensajes de éxito
+        $this->command->info('Client user creado con éxito.');
+
+
+    }
+}
